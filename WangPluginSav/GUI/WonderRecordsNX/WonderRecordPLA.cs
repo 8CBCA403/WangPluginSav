af571d03e9ea6a55fab076eba4f4c8ebec3f5261
Professor Dirty <103500840+wubinwww@users.noreply.github.com>
2024-03-15 14:05:22 +0800

    Add files via upload

--- WangPluginSav/GUI/WonderRecordsNX/WonderRecordPLA.cs
+++ WangPluginSav/GUI/WonderRecordsNX/WonderRecordPLA.cs
@@ -1111,7 +1111,7 @@ partial class WonderRecordPLA : Form
             case 0:
                 return null;
             case 1:
-                image = SpriteUtil.GetSprite(num2, b, num6, 0u, num5, flag, shiny, EntityContext.None);
+                image = SpriteUtil.GetSprite(num2, b, (byte)num6, 0u, num5, flag, shiny, EntityContext.None);
                 if (flag2)
                 {
                     Bitmap dyna = PKHeX.Drawing.PokeSprite.Properties.Resources.dyna;
